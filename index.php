58cd918078cd82f5fddee18ec8d4239c36200c08
vetalreal <vetalreal@ukr.net>
2013-02-25 18:37:14 +0200

    Произведены координальные измененния 400px (для Натальи!)

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
     <script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
     <script type="text/javascript" src="js/lightbox.js"></script>
     <link type="text/css" rel="stylesheet" href="css/lightbox.css" />
+    
     <style>
       div.resize{
-        width: 300px;
+        width: 400px;
         height: 200px;
         background-color: chartreuse;
         overflow: hidden;
       }
-      
     </style>
   </head>
   <body>
@@ -39,7 +39,7 @@
           jQuery('div.resize').stop().animate({width:miidle_size+"px"},400);
         },
         function(){
-          jQuery('div.resize').stop().animate({width:"300px"},400);
+          jQuery('div.resize').stop().animate({width:"400px"},400);
         }    
         );
     </script>
